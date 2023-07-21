7d9b9651f72bede4d615f0e7d46385bc0cd2603a
Artur Plysyuk <ifuelen@gmail.com>
2023-07-21 12:32:55 +0300

    Export formatter settings

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,8 @@
 # Used by "mix format"
+locals_without_parens = [produce: 2, update: 2, delete: 1, produce: 1, param: 2, param: 3]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [produce: 2, update: 2, delete: 1, produce: 1, param: 2, param: 3]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
