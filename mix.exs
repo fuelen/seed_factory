2ae86bae0365ac5a817a4a2e5456d48e7680e88d
Artur Plysyuk <ifuelen@gmail.com>
2026-01-28 15:36:04 +0200

    Compile files in test/support

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SeedFactory.MixProject do
       app: :seed_factory,
       version: @version,
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       consolidate_protocols: Mix.env() != :dev,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -39,6 +40,9 @@ defmodule SeedFactory.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,1 @@
-Code.require_file("support/schema_example.exs", __DIR__)
-Code.require_file("support/schema_example_extended.exs", __DIR__)
 ExUnit.start()
